dc8357012cefceb3e3a3e72bf67cde723be10696
Cornaro <luca.cornaro@accenture.com>
2026-05-27 11:00:57 +0200

    init dev branch luca.cornaro, task FE 2.01: DONE

---
+++ src/app/features/catalogo-corsi/pages/lista-corsi/lista-corsi.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lista-corsi',
+  imports: [],
+  templateUrl: './lista-corsi.html',
+  styleUrl: './lista-corsi.css',
+})
+export class ListaCorsi {}
