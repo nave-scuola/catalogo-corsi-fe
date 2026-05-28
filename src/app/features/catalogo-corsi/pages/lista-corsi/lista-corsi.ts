7b4eb1df99a1b3ea2a5023574f16d15700bc9d58
Cornaro <luca.cornaro@accenture.com>
2026-05-28 12:46:54 +0200

    cards now are customized

--- src/app/features/catalogo-corsi/pages/lista-corsi/lista-corsi.ts
+++ src/app/features/catalogo-corsi/pages/lista-corsi/lista-corsi.ts
@@ -10,4 +10,7 @@ import { Card } from '../../../../shared/components/card/card';
 
 export class ListaCorsi {
   titolo = 'Catalogo Corsi';
-}removeEventListener
+  urlPython= 'https://static.vecteezy.com/system/resources/previews/012/697/295/non_2x/3d-python-programming-language-logo-free-png.png';
+  urlJava= 'https://cdn-blog.adafruit.com/uploads/2019/03/Capture-4.jpg';
+  urlSigil= 'https://images.squarespace-cdn.com/content/v1/5fef3318f8ce9b562500206e/1638026137558-1NZMR0HJ05WGAXNJTHX6/sigil-vid.jpg';
+}
